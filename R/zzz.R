68d637617dc45241c3b4280798ed2d90d853b70f
hadrien_leclerc <hadrien.leclerc@insee.fr>
2020-07-16 13:23:28 +0200

    onLoad file : more variable to reduce number of arguments

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+
+
+.onLoad <- function(libname, pkgname){
+  Sys.setenv(INSEE_sdmx_link = "https://www.bdm.insee.fr/series/sdmx/data")
+  Sys.setenv(INSEE_website = "https://www.insee.fr/en/statistiques")
+  Sys.setenv(INSEE_idbank_file = "correspondance_idbank_dimension")
+  Sys.setenv(INSEE_idbank_path = "fichier/2868055")
+  Sys.setenv(INSEE_idbank_nchar = 9)
+  Sys.setenv(INSEE_idbank_sep = ";")
+}
