76fd971cea55d5f363bc37eff729769a5bffd9bc
hadrien_leclerc <hadrien.leclerc@insee.fr>
2020-07-20 14:36:38 +0200

    INSEE_sdmx_link correction

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 
 
 .onLoad <- function(libname, pkgname){
-  Sys.setenv(INSEE_sdmx_link = "https://www.bdm.insee.fr/series/sdmx/data")
+  Sys.setenv(INSEE_sdmx_link = "https://bdm.insee.fr/series/sdmx/data")
   Sys.setenv(INSEE_website = "https://www.insee.fr/en/statistiques")
   Sys.setenv(INSEE_idbank_file = "correspondance_idbank_dimension")
   Sys.setenv(INSEE_idbank_path = "fichier/2868055")
