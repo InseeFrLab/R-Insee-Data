11c739e3bb6dfefc539d1171862ad8d364569aa5
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2020-07-20 21:50:40 +0200

    error correction : sdmx path for dataset was wrong

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,7 @@
 .onLoad <- function(libname, pkgname){
   Sys.setenv(INSEE_sdmx_link_dataflow = "https://bdm.insee.fr/series/sdmx/dataflow")
   Sys.setenv(INSEE_sdmx_link_idbank = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM")
+  Sys.setenv(INSEE_sdmx_link_dataset = "https://bdm.insee.fr/series/sdmx/data")
   Sys.setenv(INSEE_idbank_dataset_path = "https://www.insee.fr/en/statistiques/fichier/2868055/correspondance_idbank_dimension.zip")
   Sys.setenv(INSEE_idbank_dataset_file = "correspondance_idbank_dimension")
   Sys.setenv(INSEE_idbank_nchar = 9)
