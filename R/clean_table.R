7767ca3639e5d6a5f1e452b6dd82235101a11d6f
hadrien_leclerc <hadrien.leclerc@insee.fr>
2020-08-07 11:40:20 +0200

    clean_table function to get rid of NA

---
+++ R/clean_table.R
@@ -0,0 +1,19 @@
+#' Remove columns filled only with NA (missing value)
+#'
+#' @param df the dataframe to be cleaned
+#' @examples
+#' \donttest{
+#'library(tidyverse)
+#'
+#'idbank_list = get_idbank_list() #idbank list
+#'
+#'idbank_empl = idbank_list %>%
+#'  filter(nomflow == "EMPLOI-SALARIE-TRIM-NATIONAL") %>% #employment
+#'  mutate(title = get_insee_title(idbank)) %>%
+#'  separate(title, sep = " - ", into = paste0("title", 1:5), fill = "right") %>%
+#'  clean_table()
+#' }
+#' @export
+clean_table = function(df){
+  df[, colSums(is.na(df)) != nrow(df)]
+}
