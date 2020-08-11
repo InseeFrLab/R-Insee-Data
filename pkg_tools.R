fa1ba0eeff91d63b55d4579cabbb39c73812d292
hadrien_leclerc <hadrien.leclerc@insee.fr>
2020-08-11 15:43:07 +0200

    website/doc update + description cpp11

--- pkg_tools.R
+++ pkg_tools.R
@@ -9,8 +9,6 @@ pkgdown::build_site()
 
 # Run code coverage on a single file and show a nice report
 Sys.setenv("NOT_CRAN"="true") # Make not-CRAN tests run
-covr::report(covr::file_coverage('R/read_cell_seg_data.R',
-                                 'tests/testthat/test_read_cell_seg_data.R'))
 
 # Run coverage for the entire package, tests only
 covr::report(covr::package_coverage())
