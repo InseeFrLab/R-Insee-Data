cdd5d4c5273da43946fda8db6769d3387fcb40ee
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2020-08-13 20:19:31 +0200

    website/doc update

--- doc/insee-vignettes.R
+++
@@ -1,7 +0,0 @@
-## ----setup,echo=FALSE, include=FALSE------------------------------------------
-# setup chunk
-NOT_CRAN <- identical(tolower(Sys.getenv("NOT_CRAN")),"true")
-knitr::opts_chunk$set(purl = NOT_CRAN)
-library(insee)
-library(tidyverse)
-
