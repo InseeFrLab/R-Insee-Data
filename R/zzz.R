88ccf7e4d77cdc6462be23d966c6ecf9c9a31ca9
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2020-08-31 10:26:19 +0200

    bug fix : multiple title separators

--- R/zzz.R
+++ R/zzz.R
@@ -11,8 +11,9 @@
   Sys.setenv(INSEE_get_idbank_limit = 1200)
   Sys.setenv(INSEE_sdmx_idbank_limit = 400)
   Sys.setenv(INSEE_idbank_sep = ",")
-  Sys.setenv(INSEE_title_sep = " - ")
+  Sys.setenv(INSEE_title_sep = " - | — | – ")
   Sys.setenv(INSEE_value_as_numeric = "TRUE")
   Sys.setenv(INSEE_download_verbose = "TRUE")
+  Sys.setenv(INSEE_no_cache_use = "FALSE")
   Sys.setenv(INSEE_print_query = "FALSE")
 }
