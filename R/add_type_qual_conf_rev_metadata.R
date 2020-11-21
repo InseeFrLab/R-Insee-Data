494f9bf22b413da4035631cb887bbffd7b4c9143
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2020-11-21 13:50:21 +0100

    more metadata + col title

--- R/add_type_qual_conf_rev_metadata.R
+++ R/add_type_qual_conf_rev_metadata.R
@@ -8,32 +8,9 @@ add_type_qual_conf_rev_metadata = function(df){
   df_rev = get_dimension_values("OBS_REV")
   df_conf = get_dimension_values("OBS_CONF")
 
-  # df_qual = data.frame(
-  #   OBS_QUAL = c("E", "P", "SD", "DEF", "F"),
-  #   OBS_QUAL_label_en = c("estimate", "provisional value", "semi-final value", "final value", "forecast"),
-  #   OBS_QUAL_label_fr = c("estimation", "valeur provisoire", "valeur semi-d\u00E9finitive", "valeur d\u00E9finitive", "pr\u00E9vision"),
-  #   stringsAsFactors = FALSE
-  # )
-  #
-  # df_type = data.frame(
-  #   OBS_TYPE = c("A", "U", "O", "ND", "N"),
-  #   OBS_TYPE_label_en = c("normal", "atypical", "missing", "not available", "not significant"),
-  #   OBS_TYPE_label_fr = c("normal", "atypique", "manquant", "non disponible", "non significatif"),
-  #   stringsAsFactors = FALSE
-  # )
-  # df_rev = data.frame(
-  #   OBS_REV = c(1),
-  #   OBS_REV_label_en = c("revised"),
-  #   OBS_REV_label_fr = c("r\u00E9vis\u00E9"),
-  #   stringsAsFactors = FALSE
-  # )
-  # df_conf = data.frame(
-  #   OBS_CONF = c("F", "C", "Q"),
-  #   OBS_CONF_label_en = c("public value", "confidential value", "covered by statistical confidentiality"),
-  #   OBS_CONF_label_fr = c("donn\u00E9e publique", "donn\u00E9e confidentielle", "couvert par le secret statistique"),
-  #   stringsAsFactors = FALSE
-  # )
-
+  if("OBS_STATUS" %in% names(df)){
+    df = dplyr::left_join(df, df_status, by = "OBS_STATUS")
+  }
   if("OBS_QUAL" %in% names(df)){
     df = dplyr::left_join(df, df_qual, by = "OBS_QUAL")
   }
--- R/add_type_qual_conf_rev_title.R
+++ R/add_type_qual_conf_rev_title.R
@@ -3,10 +3,34 @@
 add_type_qual_conf_rev_title = function(){
 
   df = data.frame(
-    dimension = c("OBS_QUAL", "OBS_CONF", "OBS_TYPE", "OBS_REV"),
-    label_fr = c("Qualit\u00E9", "Confidentialit\u00E9", "Type de donn\u00E9es", "R\u00E9vision"),
-    label_en = c("Quality", "Confidentiality", "Type of value", "Revision"),
+    dimension = c("FREQ",
+                  "LAST_UPDATE",
+                  "UNIT_MEASURE",
+                  "UNIT_MULT",
+                  "DECIMALS",
+                  "REF_AREA"),
+    label_fr = c("P\u00E9riodicit\u00E9",
+                 "Date de la derni\U00E8re mise \U00E0 jour",
+                 "Unit\u00E9",
+                 "Puissance",
+                 "Nombre de d\u00E9cimales",
+                 "Zone g\u00E9ographique"),
+    label_en = c("Frequency",
+                 "Last update date",
+                 "Unit",
+                 "Unit multiplier",
+                 "Decimals",
+                 "Reference Area"),
     stringsAsFactors = FALSE
   )
+
+  df_status = get_dimension_values("OBS_STATUS", name = TRUE)
+  df_qual = get_dimension_values("OBS_QUAL", name = TRUE)
+  df_type = get_dimension_values("OBS_TYPE", name = TRUE)
+  df_rev = get_dimension_values("OBS_REV", name = TRUE)
+  df_conf = get_dimension_values("OBS_CONF", name = TRUE)
+
+  df = dplyr::bind_rows(df, df_status, df_conf, df_qual, df_type, df_rev)
+
   return(df)
 }
