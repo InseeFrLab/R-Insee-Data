65282d02cf9c3e5ea8cd792117c7237546a04c78
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2020-12-29 11:10:11 +0100

    bug fix get_column_title

--- R/get_column_title.R
+++ R/get_column_title.R
@@ -16,13 +16,23 @@ get_column_title = function(dataset = NULL){
   }else{
     list_dataset = as.character(get_dataset_list()$id)
 
-    dataset_dimension = unique(unlist(
+    df_dataset_dimension = dplyr::bind_rows(
       lapply(1:length(list_dataset),
                       function(i){
+
                         dataset_dimension = get_dataset_dimension(dataset = list_dataset[i])
-                        return(dataset_dimension)
+
+                        df = data.frame(dim = as.character(dataset_dimension),
+                                        cl = attr(dataset_dimension, 'cl'),
+                                          stringsAsFactors = FALSE)
+                        return(df)
                       }
-                        )))
+                        ))
+
+    df_dataset_dimension = dplyr::distinct(.data = df_dataset_dimension)
+
+    dataset_dimension = dplyr::pull(.data = df_dataset_dimension, dim)
+    attr(dataset_dimension, 'cl') = dplyr::pull(.data = df_dataset_dimension, cl)
   }
 
   if(!is.null(dataset_dimension)){
@@ -42,6 +52,7 @@ get_column_title = function(dataset = NULL){
 
     dimension_name_df = dplyr::bind_rows(dimension_name_df, add_type_qual_conf_rev_title())
     dimension_name_df = dplyr::arrange(.data = dimension_name_df, .data$dimension)
+    dimension_name_df = dplyr::distinct(.data = dimension_name_df)
 
     dimension_name_df = tibble::as_tibble(dimension_name_df)
 
