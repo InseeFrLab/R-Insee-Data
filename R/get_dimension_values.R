d8f5fb56e8979d889adb2a03f089fea0aecff286
hadrien leclerc <hadrien.leclerc@insee.fr>
2021-01-20 21:42:04 +0100

    big dix get_dimension_values : try-error handling

--- R/get_dimension_values.R
+++ R/get_dimension_values.R
@@ -33,18 +33,20 @@ get_dimension_values = function(dimension, col_name ,name = FALSE){
 
         response_content = try(httr::content(response, encoding = "UTF-8"), silent = TRUE)
 
-        content_list = xml2::as_list(response_content)
-        data = tibble::as_tibble(content_list)
-        data_code = try(data[[1]][[2]][["Codelists"]][["Codelist"]], silent = TRUE)
+        if(class(response_content) != "try-error"){
 
-        if(class(data_code) != "try-error"){
+          content_list = xml2::as_list(response_content)
+          data = tibble::as_tibble(content_list)
+          data_code = try(data[[1]][[2]][["Codelists"]][["Codelist"]], silent = TRUE)
 
-          code_element = which(names(data_code) == "Code")
-          name_element = which(names(data_code) == "Name")
+          if(class(data_code) != "try-error"){
 
-          if(!file.exists(dimension_name_file_cache)){
-            if(length(name_element) > 0){
-              data_name = data_code[name_element]
+            code_element = which(names(data_code) == "Code")
+            name_element = which(names(data_code) == "Name")
+
+            if(!file.exists(dimension_name_file_cache)){
+              if(length(name_element) > 0){
+                data_name = data_code[name_element]
 
 
                 if(attr(data_name[[1]], "lang") == "fr"){
@@ -61,60 +63,64 @@ get_dimension_values = function(dimension, col_name ,name = FALSE){
                   dimension_label_en = "Missing"
                 }
                 dimension_name = data.frame(dimension = col_name,
-                                             label_fr = dimension_label_fr,
-                                             label_en = dimension_label_en,
-                                             stringsAsFactors = F)
+                                            label_fr = dimension_label_fr,
+                                            label_en = dimension_label_en,
+                                            stringsAsFactors = F)
 
                 saveRDS(dimension_name, file = dimension_name_file_cache)
 
+              }
+            }else{
+              dimension_name = readRDS(dimension_name_file_cache)
             }
-          }else{
-            dimension_name = readRDS(dimension_name_file_cache)
-          }
-          if(name){
-            return(dimension_name)
-          }
+            if(name){
+              return(dimension_name)
+            }
 
-          if(!name){
-            if(length(code_element) > 0){
+            if(!name){
+              if(length(code_element) > 0){
 
-              data_code = data_code[code_element]
+                data_code = data_code[code_element]
 
-              labels = dplyr::bind_rows(lapply(1:length(data_code), function(i){
+                labels = dplyr::bind_rows(lapply(1:length(data_code), function(i){
 
-                if(attr(data_code[[i]][[1]], "lang") == "fr"){
-                  label_fr = try(data_code[[i]][[1]][[1]], silent = TRUE)
-                  label_en = try(data_code[[i]][[2]][[1]], silent = TRUE)
-                }else{
-                  label_fr = try(data_code[[i]][[2]][[1]], silent = TRUE)
-                  label_en = try(data_code[[i]][[1]][[1]], silent = TRUE)
-                }
+                  if(attr(data_code[[i]][[1]], "lang") == "fr"){
+                    label_fr = try(data_code[[i]][[1]][[1]], silent = TRUE)
+                    label_en = try(data_code[[i]][[2]][[1]], silent = TRUE)
+                  }else{
+                    label_fr = try(data_code[[i]][[2]][[1]], silent = TRUE)
+                    label_en = try(data_code[[i]][[1]][[1]], silent = TRUE)
+                  }
 
-                if(class(label_fr) == "try-error"){
-                  label_fr = "Missing"
-                }
-                if(class(label_en) == "try-error"){
-                  label_en = "Missing"
-                }
+                  if(class(label_fr) == "try-error"){
+                    label_fr = "Missing"
+                  }
+                  if(class(label_en) == "try-error"){
+                    label_en = "Missing"
+                  }
 
-                df = data.frame(code = attr(data_code[[i]], "id"),
-                                label_fr = label_fr,
-                                label_en = label_en,
-                                stringsAsFactors = F)
+                  df = data.frame(code = attr(data_code[[i]], "id"),
+                                  label_fr = label_fr,
+                                  label_en = label_en,
+                                  stringsAsFactors = F)
 
-                return(df)
-              }))
+                  return(df)
+                }))
 
-              names(labels) = c(col_name, paste0(col_name, "_label_fr"), paste0(col_name, "_label_en"))
+                names(labels) = c(col_name, paste0(col_name, "_label_fr"), paste0(col_name, "_label_en"))
 
-              saveRDS(labels, file = dimension_file_cache)
+                saveRDS(labels, file = dimension_file_cache)
 
-              return(labels)
+                return(labels)
 
-            }else{return(NULL)}
-          }
+              }else{return(NULL)}
+            }
+
+          }else{return(NULL)}
 
         }else{return(NULL)}
+
+
 
     }else{return(NULL)}
 
