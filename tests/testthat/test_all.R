8c828b7308d0b0f61b8d93644f1231504a6de0e0
hadrien leclerc <hadrien.leclerc@insee.fr>
2021-01-26 12:44:23 +0100

    bug fix : error message about dataset filters in get_insee_dataset

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -10,8 +10,6 @@ test_that("class tests",{
   expect_warning(create_insee_folder(), regexp = NA)
   expect_warning(insee:::.onLoad(), regexp = NA)
 
-  expect_equal(read_dataset_metadata("CLIMAT-AFFAIRES"), TRUE)
-
   INSEE_idbank_dataset_path = Sys.getenv("INSEE_idbank_dataset_path")
   Sys.setenv(INSEE_idbank_dataset_path = "https://www.insee.fr/en/statistiques/fichier/fake_file.zip")
   expect_equal(any(class(get_idbank_list("CLIMAT-AFFAIRES")) == 'data.frame'), TRUE)
@@ -150,6 +148,7 @@ test_that("output tests",{
   expect_equal(all(names(df_slow) == names(df_fast)), TRUE)
 
   expect_warning(clean_insee_folder(), regexp = NA)
+  expect_equal(read_dataset_metadata("CLIMAT-AFFAIRES"), TRUE)
   expect_equal('data.frame' %in% class(get_dimension_values('CL_NATURE', 'NATURE', name = TRUE)), TRUE)
 
 })
