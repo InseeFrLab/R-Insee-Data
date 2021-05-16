9b7f9337c14f9024694fc186b7cd76feec1508b5
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-05-16 20:50:26 +0200

    create_insee_folder no do create specific data folder

--- R/create_insee_folder.R
+++ R/create_insee_folder.R
@@ -2,8 +2,9 @@
 create_insee_folder = function(){
 
   insee_folder = file.path(rappdirs::user_data_dir(), "insee")
-  list_folders = c(insee_folder, file.path(insee_folder, "insee"),
-                   file.path(insee_folder, "insee", "data"))
+  list_folders = c(insee_folder, file.path(insee_folder, "insee")
+                   # ,file.path(insee_folder, "insee", "data")
+                   )
 
   for(ifile in 1:length(list_folders)){
     if(!file.exists(list_folders[ifile])){
@@ -17,18 +18,18 @@ create_insee_folder = function(){
     saveRDS(test, file = test_file)
   }
 
-  # files deleted after 2 hours
-  list_file_insee_data_dir = file.path(file.path(insee_folder, "insee", "data"),
-                                       list.files(path = file.path(insee_folder, "insee", "data")))
-
-  date_file_modif = file.info(list_file_insee_data_dir)$mtime
-  diff_time_files = difftime(Sys.time(), date_file_modif, units = "hours")
-
-  old_files = list_file_insee_data_dir[diff_time_files > 2]
-
-  # file deletion
-  if(length(old_files) > 0){
-    invisible(file.remove(old_files))
-  }
+  # # files deleted after 2 hours
+  # list_file_insee_data_dir = file.path(file.path(insee_folder, "insee", "data"),
+  #                                      list.files(path = file.path(insee_folder, "insee", "data")))
+  #
+  # date_file_modif = file.info(list_file_insee_data_dir)$mtime
+  # diff_time_files = difftime(Sys.time(), date_file_modif, units = "hours")
+  #
+  # old_files = list_file_insee_data_dir[diff_time_files > 2]
+  #
+  # # file deletion
+  # if(length(old_files) > 0){
+  #   invisible(file.remove(old_files))
+  # }
 
 }
