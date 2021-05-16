133da317a7518905d1357a0ab3948a91116b4847
hadrien_leclerc <hadrien.leclerc@insee.fr>
2021-05-16 19:42:33 +0200

    numeric data back in temp directory

--- R/get_last_release.R
+++ R/get_last_release.R
@@ -11,13 +11,15 @@ get_last_release = function(){
 
   insee_last_release_link = Sys.getenv("INSEE_last_release_link")
 
-  dir_creation_fail = try(create_insee_folder(), silent = TRUE)
+  # dir_creation_fail = try(create_insee_folder(), silent = TRUE)
+  #
+  # if(!"try-error" %in% class(dir_creation_fail)){
+  #   insee_data_dir = file.path(rappdirs::user_data_dir("insee"), "data")
+  # }else{
+  #   insee_data_dir = tempdir()
+  # }
 
-  if(!"try-error" %in% class(dir_creation_fail)){
-    insee_data_dir = file.path(rappdirs::user_data_dir("insee"), "data")
-  }else{
-    insee_data_dir = tempdir()
-  }
+  insee_data_dir = tempdir()
 
   file_cache = file.path(insee_data_dir, paste0(openssl::md5(insee_last_release_link), ".rds"))
 
