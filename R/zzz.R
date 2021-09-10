bfb94963f2e71c30c6db29635a0549183db98b33
Hadrien Leclerc <leclerc.hadrien@gmail.com>
2021-09-10 10:08:46 +0200

    idbank file change

--- R/zzz.R
+++ R/zzz.R
@@ -8,13 +8,9 @@
   Sys.setenv(INSEE_sdmx_link_idbank = "https://bdm.insee.fr/series/sdmx/data/SERIES_BDM")
   Sys.setenv(INSEE_sdmx_link_dataset = "https://bdm.insee.fr/series/sdmx/data")
 
-  # Sys.setenv(INSEE_idbank_dataset_path = "https://www.insee.fr/en/statistiques/fichier/2868055/2020_correspondance_idbank_dimension.zip")
-  # Sys.setenv(INSEE_idbank_dataset_file = "2020_correspondances_idbank_dimension")
-  # Sys.setenv(INSEE_idbank_sep = ",")
-
   Sys.setenv(INSEE_idbank_sep = ";")
-  Sys.setenv(INSEE_idbank_dataset_path = "https://www.insee.fr/en/statistiques/fichier/2868055/2021_correspondance_idbank_dimension.zip")
-  Sys.setenv(INSEE_idbank_dataset_file = "2021_correspondance_idbank_dimension")
+  Sys.setenv(INSEE_idbank_dataset_path = "https://www.insee.fr/en/statistiques/fichier/2868055/202108_correspondance_idbank_dimension.zip")
+  Sys.setenv(INSEE_idbank_dataset_file = "202108_correspondance_idbank_dimension")
 
   Sys.setenv(INSEE_last_release_link = "https://bdm.insee.fr/series/sdmx/rss/donnees")
   Sys.setenv(INSEE_idbank_nchar = 9)
